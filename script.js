54b1541ddb4f01ae88ce31a86f26f60f3e3841ab
Jonatan Helander <Jonatanhelander@hotmail.com>
2023-10-31 09:54:19 +0100

    Testing creating branch and saved float buttons

--- script.js
+++ script.js
@@ -4,9 +4,24 @@ const state =  {
     inventory: ["knife", "gun"],
     level: 1
 }
-function replaceBox(box) {
-    const newBox = document.createElement("div")
-    newBox.gameBox = "gameBox";
-    newBox.innerText = "Do you want to play a game?"
+function changeTextAndButtons() {
+    const content = document.getElementById("changeTextButton");
+    content.innerHTML = 
+    `<p>Do you want to play a game?</p>`
+    const yesButtonContainer = document.createElement("div");
+    yesButtonContainer.innerHTML = `<div class="green" id="yesButton" onclick="onYesClick()">Yes</div>`
+    yesButtonContainer.classList.add("clear"); //used to clear float value
+
+    const noButtonContainer = document.createElement("div");
+    noButtonContainer.innerHTML = `<div class="red" id="noButton" onclick="onNoClick()">No</div>`
+    noButtonContainer.classList.add("clear"); //used to clear float value
+
+    document.body.appendChild(noButtonContainer);
+    document.body.appendChild(yesButtonContainer);
+}
+function onYesClick() {
+
+}
+function onNoClick() {
     
 }
